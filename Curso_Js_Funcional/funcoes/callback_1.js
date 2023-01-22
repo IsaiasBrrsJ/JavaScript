39bf976f1f84001ef13fd6ac8ef7da803f362c65
Isaias de Jesus Barros <isaiasjbarross@gmail.com>
2023-01-22 13:00:45 -0400

    callback

---
+++ Curso_Js_Funcional/funcoes/callback_1.js
@@ -0,0 +1,17 @@
+// normalmente callback está associado a eventos
+
+
+// function exec(fn, a, b) {
+//     fn(a, b);
+// }
+// const somarNoTerminal = (x, y) => console.log(x + y);
+// const subtrairNoTerminal = (x, y) => console.log(x - y);
+
+// exec(somarNoTerminal, 56, 38);
+// exec(subtrairNoTerminal, 182, 27);
+
+// setInterval(() => {
+//     console.log("isaias");
+// }, 1000); // vai ficar executando no intervalo de 1 segundo
+
+
